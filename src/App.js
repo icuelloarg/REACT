d7efd9e450770c11649702d10a231ed0c0bc5b7f
icuelloarg <icuelloarg@gmail.com>
2022-02-07 22:19:49 -0300

    entrega cartWidget y listContainer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import '././sass/app.scss';
+import ItemListContainer from './components/itemListContainer/ItemListContainer.jsx';
 import Navbar from './components/navbar/Navbar.jsx';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <Navbar/>
+      <ItemListContainer/>
       </header>
     </div>
   );
